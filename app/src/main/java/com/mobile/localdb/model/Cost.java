6af6715435f062cb19831f43d311e75f92f91322
Komarov Nikolay <komarov.nikolay.russia@gmail.com>
2017-10-26 11:56:04 +0300

    Create listview and header of the table.

---
+++ app/src/main/java/com/mobile/localdb/model/Cost.java
@@ -0,0 +1,40 @@
+package com.mobile.localdb.model;
+
+public class Cost {
+    private String name;
+    private boolean isAsset;
+    private Double amount;
+    private int version;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isAsset() {
+        return isAsset;
+    }
+
+    public void setAsset(boolean asset) {
+        isAsset = asset;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+}
