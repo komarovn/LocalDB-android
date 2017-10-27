c607f18b172728fba976fd736e76f56c3545026c
Komarov Nikolay <komarov.nikolay.russia@gmail.com>
2017-10-27 09:42:44 +0300

    Process total amount and show on screen.

--- app/src/main/java/com/mobile/localdb/TotalActivity.java
+++ app/src/main/java/com/mobile/localdb/TotalActivity.java
@@ -4,18 +4,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
+
+import com.mobile.localdb.model.Cost;
+import com.mobile.localdb.model.DatabaseOperations;
 
 public class TotalActivity extends AppCompatActivity {
+    private DatabaseOperations db = DatabaseOperations.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.total_layout);
+        TextView total = (TextView) findViewById(R.id.totalCountLabel);
+        total.setText(String.valueOf(getTotal()) + " $");
     }
 
     public void onClosingTotal(View view) {
         Intent intent = new Intent();
         setResult(RESULT_OK, intent);
         finish();
+    }
+
+    public double getTotal() {
+        double total = 0.0;
+        for (Cost cost : db.getData()) {
+            total += cost.getAmount();
+        }
+        return total;
     }
 }
